53f877880cf39ed180ed0b254d90eecb79069e41
brandonsueur <gynidark@gmail.com>
2018-05-03 16:02:16 +0200

    :rocket: Create component 'Banner'

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 import Header from './components/Header/Header'
+import Banner from './components/Banner/Banner'
 
 class App extends Component {
   render() {
     return (
         <div>
             <Header/>
+
+            <Banner />
+
+            <hr />
         </div>
     );
   }
