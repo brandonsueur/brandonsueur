7a769bd2a82974c11bb5e4db4cca5d58488de027
brandonsueur <gynidark@gmail.com>
2018-05-07 14:58:49 +0200

    :bug: Change adress mail

--- app/src/components/Contact/Contact.js
+++ app/src/components/Contact/Contact.js
@@ -4,6 +4,34 @@ import React, { Component } from 'react'
 import Socials from '../Socials'
 
 class Contact extends Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            error: null,
+            isLoaded: false,
+            mail: null
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3001/infos")
+            .then(res => res.json())
+            .then(
+                result => {
+                    this.setState({
+                        isLoaded: true,
+                        mail: result[0].value
+                    })
+                },
+                error => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    })
+                }
+            )
+    }
 
     render() {
         return(
@@ -12,7 +40,7 @@ class Contact extends Component{
 
                     <h2>Vous êtes intéressé à travailler avec moi ?<span>N'hésitez pas  à m'écrire !</span></h2>
 
-                    <p>Écrivez-moi directement sur mon e-mail — <a href="mailto:brandon.sueur@outlook.fr">brandon.sueur@outlook.fr</a> ou n'hésitez pas à visiter mes différents réseaux sociaux ci-dessous. Vous aurez une réponse dans les plus brefs délais.</p>
+                    <p>Écrivez-moi directement sur mon e-mail — <a href={ "mailto:" + this.state.mail}>{this.state.mail}</a> ou n'hésitez pas à visiter mes différents réseaux sociaux ci-dessous. Vous aurez une réponse dans les plus brefs délais.</p>
 
                     <Socials />
                 </div>
