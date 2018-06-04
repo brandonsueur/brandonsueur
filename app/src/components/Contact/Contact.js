6f2327def5f1fb698b88394f914838fd6676cc23
brandonsueur <gynidark@gmail.com>
2018-06-04 22:09:23 +0200

    (feat): edit component 'Contact'

--- app/src/components/Contact/Contact.js
+++ app/src/components/Contact/Contact.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 
 import Socials from '../Socials'
 
+import "./Contact.css"
+
 class Contact extends Component{
     constructor(props) {
         super(props)
