16cec28c9adc2ba77bfd0af22b20fec085ef3908
brandonsueur <gynidark@gmail.com>
2018-06-05 09:07:54 +0200

    (feat): new call file CSS

--- app/src/index.js
+++ app/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import "./App.css"
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
