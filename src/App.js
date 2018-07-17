c5141b69d9e9c57b714e8c0e8abac70bc23bfd22
brandonsueur <gynidark@gmail.com>
2018-07-17 20:46:12 +0200

    feat: return true

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        {/* your code ... */}
-      </div>
-    );
+    return true
   }
 }
 
