0fb3d08a4270c0b502334b044449e7c8ac7af1f0
brandonsueur <gynidark@gmail.com>
2018-07-18 15:35:42 +0200

    feat: create element <main/>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,14 +8,14 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <app>
+      <app className="wrapper">
         <div className="split-background">
           <Background />
         </div>
 
-        <div className="split-container">
-          {/* the code ... */}
-        </div>
+        <main className="split-container">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque assumenda adipisci fugit, numquam placeat eius doloribus praesentium quo, magnam laborum quas beatae fuga explicabo earum quod sunt reiciendis optio porro, odio tempore eveniet molestiae sed. Quasi ducimus ea unde, obcaecati recusandae asperiores, facilis quas exercitationem voluptatum saepe magnam! Neque nihil suscipit unde esse accusantium necessitatibus, id, aut eius perferendis facilis ipsa molestias accusamus laudantium! Molestiae fuga sint beatae eveniet, aperiam error reprehenderit. Facere ea sed quos in voluptatibus doloribus, voluptate. Vero placeat illo, minima! Rem id sapiente, dolores, fugiat neque, voluptates provident placeat consequuntur repellendus quidem aspernatur corporis maxime asperiores.
+        </main>
       </app>
     )
   }
