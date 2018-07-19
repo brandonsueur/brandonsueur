fd7e99445aa22c798dd681df399db1eb25974b0e
brandonsueur <gynidark@gmail.com>
2018-07-19 10:52:53 +0200

    feat: create component <Socials />

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Background from "../Background/Background";
-
+import Socials from "../Socials/Socials";
 
 import "./App.css";
 
@@ -14,6 +14,8 @@ class App extends Component {
         </div>
 
         <main className="split-container">
+          <Socials />
+
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque assumenda adipisci fugit, numquam placeat eius doloribus praesentium quo, magnam laborum quas beatae fuga explicabo earum quod sunt reiciendis optio porro, odio tempore eveniet molestiae sed. Quasi ducimus ea unde, obcaecati recusandae asperiores, facilis quas exercitationem voluptatum saepe magnam! Neque nihil suscipit unde esse accusantium necessitatibus, id, aut eius perferendis facilis ipsa molestias accusamus laudantium! Molestiae fuga sint beatae eveniet, aperiam error reprehenderit. Facere ea sed quos in voluptatibus doloribus, voluptate. Vero placeat illo, minima! Rem id sapiente, dolores, fugiat neque, voluptates provident placeat consequuntur repellendus quidem aspernatur corporis maxime asperiores.
         </main>
       </app>
