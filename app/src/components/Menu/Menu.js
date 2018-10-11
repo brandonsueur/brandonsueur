909912f77d863ca3cd6a84192837f6213b6d6db4
brandonsueur <gynidark@gmail.com>
2018-10-11 08:42:41 +0200

    feat(app): create className='active' menu

--- app/src/components/Menu/Menu.js
+++ app/src/components/Menu/Menu.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import Socials from "../Socials/Socials"
 
 import "./Menu.css";
+
+const oddEvent = (match, location) => {
+  if (!match) {
+    return false
+  }
+  const eventID = parseInt(match.params.eventID)
+  return !isNaN(eventID) && eventID % 2 === 1
+}
 
 class Menu extends React.Component {
   render() {
     return(
       <nav className="menu">
         <ul className="links">
-          <li><Link to={'/a-propos'}>À propos</Link></li>
-          <li><Link to={'/articles'}>Articles</Link></li>
-          <li><Link to={'/contact'}>Contact</Link></li>
+          <li><NavLink activeClassName="active" to={'/a-propos'}>À propos</NavLink></li>
+          <li><NavLink activeClassName="active" to={'/articles'}>Articles</NavLink></li>
+          <li><NavLink activeClassName="active" to={'/contact'}>Contact</NavLink></li>
         </ul>
 
         <div className="align-right">
