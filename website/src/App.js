f1bb40b51e39961b32e6ca2972a65a6c11cdba86
brandonsueur <gynidark@gmail.com>
2019-07-14 21:56:39 +0200

    feat(website): add starter project react

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World !
+        </p>
+      </header>
+    </div>
+  );
+}
