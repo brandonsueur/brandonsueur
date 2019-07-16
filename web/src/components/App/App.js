23579d9c6a156e900b29ba5f90193dded5261b71
brandonsueur <gynidark@gmail.com>
2019-07-16 21:46:14 +0200

    feat(web): config css

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -5,7 +5,11 @@ import './App.scss';
 export default function App() {
   return (
     <div className="container">
-      <h1>Welcome!</h1>
+      <h1>
+        Je conçois de jolis sites web
+        <br />
+        pour agences et particuliers !
+      </h1>
     </div>
   );
 }
