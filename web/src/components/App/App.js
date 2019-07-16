1bc743b4e4a6ddb7eb87c01a3e11951584d11e38
brandonsueur <gynidark@gmail.com>
2019-07-17 00:28:56 +0200

    feat(web): add component Socials

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
+import Socials from '../Socials/Socials';
+
 import './App.scss';
 
 export default function App() {
   return (
     <div className="container">
-      <h1>
-        Je conçois de jolis sites web
-        <br />
-        pour agences et particuliers !
-      </h1>
+      <Socials darked />
+
+      <h2>Salut tout le monde</h2>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis ut
+        maiores nisi consequatur repudiandae placeat, facere eum laboriosam id
+        illo suscipit cumque maxime tempora modi, ipsam quod laborum temporibus
+        iusto.
+      </p>
     </div>
   );
 }
