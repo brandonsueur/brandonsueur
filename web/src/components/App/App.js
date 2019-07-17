5f2bfcdc2c98cef22c7528bf0ef58a06edbbdccc
brandonsueur <gynidark@gmail.com>
2019-07-17 16:17:46 +0200

    feat(web): add banner and my picture

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 import Header from '../Header/Header';
-import Socials from '../Socials/Socials';
 import Button from '../Button/Button';
+import Socials from '../Socials/Socials';
 
 import './App.scss';
 
 export default function App() {
   return (
-    <div className="container">
+    <div className="app">
       <Header />
 
-      <Button text="Découvrir mes projets" link="http://google.fr/" />
+      <div className="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est fuga
+        reprehenderit ratione optio qui eum, minus non! Sed labore commodi et
+        error magnam quia deserunt, vitae ex illo dolores voluptatum.
+      </div>
     </div>
   );
 }
---
+++ web/src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Socials from '../Socials/Socials';
+import Button from '../Button/Button';
+
+import './Banner.scss';
+
+import imageMe from './img/me.png';
+
+export default function Banner() {}
