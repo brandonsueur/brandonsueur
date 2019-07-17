c6d00d686e74db7d113917639fe90e9386fa2b74
brandonsueur <gynidark@gmail.com>
2019-07-17 16:18:20 +0200

    feat(web): arrow function component Button

--- web/src/components/Button/Button.js
+++ web/src/components/Button/Button.js
@@ -2,28 +2,26 @@ import React from 'react';
 
 import './Button.scss';
 
-class Button extends React.PureComponent {
-  render() {
-    const { text, link } = this.props;
-    return (
-      <a className="button" href={link}>
-        {text}
+const Button = props => {
+  const { text, link } = props;
+  return (
+    <a className="button" href={link}>
+      {text}
 
-        <svg width="29" height="13">
-          <g
-            stroke="#FFF"
-            strokeWidth="2"
-            fill="none"
-            fillRule="evenodd"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M1 6.5h26M22 1l6 5.5-6 5.5" />
-          </g>
-        </svg>
-      </a>
-    );
-  }
-}
+      <svg width="29" height="13">
+        <g
+          stroke="#FFF"
+          strokeWidth="2"
+          fill="none"
+          fillRule="evenodd"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M1 6.5h26M22 1l6 5.5-6 5.5" />
+        </g>
+      </svg>
+    </a>
+  );
+};
 
 export default Button;
