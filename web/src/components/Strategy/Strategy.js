2ea3ea0fa11c8401911d96fac37ddec1333e7cbb
brandonsueur <gynidark@gmail.com>
2019-08-03 02:00:43 +0200

    feat(web): create block for component Strategy

--- web/src/components/Strategy/Strategy.js
+++ web/src/components/Strategy/Strategy.js
@@ -7,7 +7,7 @@ const Strategy = () => {
     <section className="strategy">
       <div className="container">
         <div className="row">
-          <div className="col-md-5">
+          <div className="col-sm-12 col-md-5">
             <div className="title">
               <span>Les étapes importantes</span>
               <h1>
@@ -25,6 +25,41 @@ const Strategy = () => {
               processus. Donner la priorité aux besoins de mes clients est la
               clé pour établir des relations stables et solides.
             </p>
+
+            <a
+              href="https://github.com/brandonsueur"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Réaliser un devis
+            </a>
+          </div>
+
+          <div className="col-md-offset-1 col-md-6">
+            <div className="block">
+              <h3>1. Stratégie</h3>
+              <p>
+                Établissez les objectifs du projet, les maquettes mais aussi un
+                petit cahier des charges. L'objectif étant de créer des
+                solutions rentables.
+              </p>
+            </div>
+
+            <div className="block">
+              <h3>2. Réalisation</h3>
+              <p>
+                J'ai les connaissances et l'attitude nécessaires pour mener à
+                bien un projet de A à Z. En apprenant sans cesse !
+              </p>
+            </div>
+
+            <div className="block">
+              <h3>3. Livraison</h3>
+              <p>
+                L'objectif étant d'utiliser des outils efficasses et
+                compréhensible. Cela m'a valu des clients réguliers.
+              </p>
+            </div>
           </div>
         </div>
       </div>
