14a0e399792cca33511b4d3942e2b94ef1b59778
Brandon Sueur <brandon.sueur@outlook.fr>
2021-02-03 23:08:20 +0100

    feat: add components

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,47 @@
-// import "tailwindcss/tailwind.css";
+import Head from "next/head";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+  const og = pageProps.data?.og;
+  const title = pageProps.data?.title;
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta property="og:title" content={title || `Telmo, code & design`} />
+        <meta property="og:site_name" content="Telmo, code & design" />
+        <meta
+          property="og:description"
+          content={
+            og
+              ? og.description
+              : `Writing about the tips I usually share on Twitter and some more.`
+          }
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@telmo" />
+        <meta
+          property="og:image"
+          content={og ? og.image : `https://telmo.im/og/default.png`}
+        />
+        <meta name="monetization" content="$ilp.uphold.com/iRX2HpJ4FRQ3" />
+
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+        ></script>
+
+        <title>{title || `Telmo, code & design`}</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default App;
--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -7,8 +7,16 @@ function PostTemplate({ content, data }) {
 
   return (
     <>
-      <h1>{frontmatter.title}</h1>
-      <ReactMarkdown source={content} />
+      <h1 className="mt-32 text-center leading-tight mb-20 font-black xl:text-6xl lg:text-6xl text-4xl tracking-tight text-black dark:text-white">
+        {frontmatter.title}
+      </h1>
+
+      <div className="xl:w-7/12 lg:w-9/12 mx-auto">
+        <p className="mb-6 text-sm text-gray-400 dark:text-gray-700">
+          20 Janvier 2020
+        </p>
+        <ReactMarkdown source={content} />
+      </div>
     </>
   );
 }
