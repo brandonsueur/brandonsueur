3a69c9bc433395f5fa1a24d9b0ccd5b3119e6d85
Brandon Sueur <brandon.sueur@outlook.fr>
2021-02-15 16:15:49 +0100

    fix: slug with not content

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,18 @@ const Homepage = (props) => {
       </h2>
 
       <div>
-        {data.map((post) => {
-          const url = `/writing/${post.slug}`;
-          const { title, date } = post.header;
+        {data.map((post, i) => {
+          const { title } = post.header;
 
           return (
-            <div className="flex my-5">
-              <Link href={url}>
+            <div className="flex my-5" key={i}>
+              <Link
+                href={{
+                  pathname: "/writing/[slug]",
+                  query: { slug: post.slug },
+                }}
+                key={i}
+              >
                 <a className="text-lg text-indigo-600 border-b-2 border-white hover:border-indigo-600">
                   {title}
                 </a>
--- utils/getPosts.js
+++ utils/getPosts.js
@@ -15,15 +15,14 @@ const usePosts = () => {
         const values = keys.map(context);
 
         const data = keys.map((key, index) => {
-          // Create slug from filename
           const slug = key
             .replace(/^.*[\\\/]/, "")
             .split(".")
             .slice(0, -1)
             .join(".");
           const value = values[index];
-          // Parse yaml metadata & markdownbody in document
           const document = matter(value.default);
+
           return {
             header: document.data,
             body: document.content,
