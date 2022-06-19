55d1d8d0d3ec6ac7b0e8422a2c284987de8d2365
Brandon Sueur <gynidark@gmail.com>
2022-06-20 00:17:47 +0200

    feat: showMore description of me

--- sections/me.js
+++ sections/me.js
@@ -29,7 +29,7 @@ const Me = () => {
           }}>
           <Button>
             <div className="mx-4">
-              500€ <span className="text-sm font-normal">/ jour</span>
+              600€ <span className="text-sm font-normal">/ jour</span>
             </div>
           </Button>
         </div>
@@ -40,7 +40,7 @@ const Me = () => {
 
         <div className="relative w-full">
           <p
-            style={{ height: "396px" }}
+            style={{ height: showMore ? "100%" : "396px" }}
             className="mt-8 overflow-hidden font-body font-light leading-8 text-blue-gray">
             Mon expertise couvre l'ensemble du processus de conception de
             produits numériques pour les sites web et les applications mobiles,
@@ -57,14 +57,23 @@ const Me = () => {
             réaliser du travail de qualité, je contribue régulièrement à des
             projets open-source et suis toujours à la recherche des meilleures
             pratiques, outils et standards.
+            <br />
+            Je me suis spécialisé dans les technologies web et j'ai développé
+            une expertise sur React.js, Next.js/React Native, Expo et les
+            interfaces UI/UX. Je suis plutôt spécialisé Front-End, mais aussi à
+            l'aise avec le Back-End en Laravel/Node.js. Rigoureux et soucieux de
+            réaliser du travail de qualité, je contribue régulièrement à des
+            projets open-source et suis toujours à la recherche des meilleures
+            pratiques, outils et standards.
           </p>
 
           <div
             style={{
-              background:
-                "linear-gradient(to bottom, rgba(17,19,30,0),rgba(17,19,30,0.4), rgba(17,19,30,1))",
+              background: showMore
+                ? "none"
+                : `linear-gradient(to bottom, rgba(17,19,30,0),rgba(17,19,30,0.4), rgba(17,19,30,1))`,
               position: "absolute",
-              bottom: "15px",
+              bottom: "0px",
               padding: "15px 0",
               height: "170px",
               width: "100%"
@@ -75,7 +84,7 @@ const Me = () => {
         <button
           onClick={() => setShowMore(!showMore)}
           className="mt-2 mb-8 text-left font-bold text-white underline opacity-100 transition transition-opacity ease-in-out hover:opacity-80">
-          En savoir plus
+          En savoir {showMore ? "moins" : "plus"}
         </button>
 
         <div className="mt-4 flex">
